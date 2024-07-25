d6fde743aa1431e6055358e89623d8ce542b7345
Malcolm Caleb <malcolmcaleb01@gmail.com>
2024-07-25 14:39:49 -0400

    gasp timeline

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,6 +1,41 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+  // timeline is used to manage multiple animations
+  // it similar to to, from, and fromTO but the difference is it can manage all of them
+  // when creating a timeline the first thing you have to do is define it
+  // then you give it the options object to give it properties
+  const timeline = gsap.timeline({
+    repeat: -1, repeatDelay: 1, yoyo: true
+  })
 
+  useGSAP(() => {
+    timeline.to('#yellow-box', {
+      x: 250,
+      rotation: 360,
+      borderRadius: '100%',
+      duration: 2,
+      ease: 'back.inOut'
+    })
+    timeline.to('#yellow-box', {
+      y: 250,
+      scale:2,
+      rotation: 360,
+      borderRadius: '100%',
+      duration: 2,
+
+    })
+    timeline.to('#yellow-box', {
+      x:500,
+      scale: 1,
+      rotation: 360,
+      borderRadius: '8px',
+      duration: 2,
+      ease: 'back.inOut'
+    })
+  }, [])
   return (
     <main>
       <h1>GsapTimeline</h1>
@@ -35,7 +70,14 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          // can manipulate and pause/play the animation whenever you want
+          if (timeline.paused()) {
+            timeline.play();
+          } else{
+            timeline.pause();
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
